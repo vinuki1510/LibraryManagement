62957cf04db8ab0138a21fd43670288d59532b0e
vinuki1510 <ac159059@avcol.school.nz>
2026-07-28 00:02:25 -0700

    Add ConsoleView input and message display methods.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using LibraryManagement.controller;
+using LibraryManagement.View;
 
 namespace LibraryManagement
 {
     internal class Program
     {
         private static StorageManager storageManager;
+        private static ConsoleView myView;
         static void Main(string[] args)
         {
 
@@ -12,10 +14,31 @@ namespace LibraryManagement
 
             storageManager = new StorageManager(connectionString);
             myView = new ConsoleView();
+            bool exit = false;
+            while (!exit)
+            {
+                myView.DisplayMainMenu();
+                string choice = myView.GetUserInput();
+
+                switch (choice)
+                {
+                    case "1":
+                        Login();
+                        break;
+
+                    case "3":
+                        exit = true;
+                        break;
+
+                    default:
+                        myView.DisplayMessage("Invalid choice. Please try again.");
+                        break;
+
+                }
+
+            }
 
         }
-
-
 
     }
 
---
+++ View/ConsoleView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.View
+{
+    public class ConsoleView
+    {
+        public void DisplayMainMenu()
+        {
+            Console.WriteLine("Welcome to the Library Management System");
+            Console.WriteLine("1. Manage Members");
+            Console.WriteLine("2. Manage Books");
+            Console.WriteLine("3. Manage Loans");
+            Console.WriteLine("4. Exit");
+            Console.Write("Please enter your choice: ");
+        }
+
+        public void DisplayMessage (string message)
+        {
+            Console.WriteLine(message);
+        }
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+        public int GetIntInput()
+        {
+           return int.Parse(Console.ReadLine());
+        }
+    }
+}
